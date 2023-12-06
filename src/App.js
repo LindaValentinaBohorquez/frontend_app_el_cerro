25aec9e56db887424c25476a99cf7327f5da1445
LindaBohorquez <lindavalentinabh@ufps.edu.co>
2023-12-05 20:05:15 -0500

    creacion de componentes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const data = {
+    "store": "El cerro",
+    "address": "Av 0 centro",
+    "products": [
+        {
+            "name": "producto1"
+        },
+        {
+            "name": "producto2"
+        },
+        {
+            "name": "producto3"
+        }
+    ]
+}
+  console.log("fuera",data)
   return (
     <div className="App">
       <header className="App-header">
