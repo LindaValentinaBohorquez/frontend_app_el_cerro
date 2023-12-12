3c648c3562eda4af8a176449615028496d95c1e5
LindaBohorquez <lindavalentinabh@ufps.edu.co>
2023-12-11 21:53:54 -0500

    actualizacion

--- src/components/shared/footer/Footer.js
+++ src/components/shared/footer/Footer.js
@@ -5,29 +5,29 @@ function Footer() {
   return (
     <div className="footer">
       <div className="footer-content">
-        <div className="logo-container">
-          <img src="https://i.postimg.cc/zG85wrd3/El-Cerro.png" alt="Logo del supermercado El Cerro" className="logo-image" />
+        <div className="footer-logo-container">
+          <img src="https://i.postimg.cc/zG85wrd3/El-Cerro.png" alt="Logo del supermercado El Cerro" className="footer-logo-image" />
         </div>
-        <div className="contact-info">
+        <div className="footer-contact-info">
           <p>¿Necesitas ayuda?</p>
-          <p>Contáctanos al <span className="phone-number">300 - 0000000</span></p>
+          <p>Contáctanos al <span className="footer-phone-number">300 - 0000000</span></p>
           <p>o síguenos en nuestras redes sociales</p>
-          <div className="social-icons">
+          <div className="footer-social-icons">
             <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
-              <img src="https://i.postimg.cc/90q24JXH/twitter.png" alt="Twitter" className="icon social-icon1" />
+              <img src="https://i.postimg.cc/90q24JXH/twitter.png" alt="Twitter" className="icon footer-social-icon1" />
             </a>
             <a href="https://www.youtube.com/channel/UCP_c3w64Eh_foIcejqc31xw" target="_blank" rel="noopener noreferrer">
-              <img src="https://i.postimg.cc/QdrSYTxq/youtube.png" alt="YouTube" className="icon social-icon2" />
+              <img src="https://i.postimg.cc/QdrSYTxq/youtube.png" alt="YouTube" className="icon footer-social-icon2" />
             </a>
             <a href="https://www.instagram.com/_linda_memories_/" target="_blank" rel="noopener noreferrer">
-              <img src="https://i.postimg.cc/KvhswV0L/instagram.png" alt="Instagram" className="icon social-icon3" />
+              <img src="https://i.postimg.cc/KvhswV0L/instagram.png" alt="Instagram" className="icon footer-social-icon3" />
             </a>
             <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-              <img src="https://i.postimg.cc/d1Jzbfs4/fb.png" alt="Facebook" className="icon social-icon4" />
+              <img src="https://i.postimg.cc/d1Jzbfs4/fb.png" alt="Facebook" className="icon footer-social-icon4" />
             </a>
           </div>
         </div>
-        <div className="about-us">
+        <div className="footer-about-us">
           <p>Nuestra empresa</p>
           <p>Somos una empresa familiar, proveniente del campo de nuestra querida Colombia, llevando a las casas de los colombianos los mejores productos que ofrece el mercado</p>
         </div>
