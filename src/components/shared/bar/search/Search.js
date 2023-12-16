52bcde2ffac8cde6883470eac0e25cf77e6c9cce
LindaBohorquez <lindavalentinabh@ufps.edu.co>
2023-12-16 10:03:57 -0500

    se realizo las funciones para el search

--- src/components/shared/bar/search/Search.js
+++ src/components/shared/bar/search/Search.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import "./Search.css"
+
 function Search() {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+      };
+
+    function searchByString() {
+        const listProducts = JSON.parse(localStorage.getItem('allProducts'))
+        const results = listProducts.filter(object => {
+            const nameMatch = object.nombre.toLowerCase().includes(inputValue.toLowerCase());
+            const descriptionMatch = object.descripcion.toLowerCase().includes(inputValue.toLowerCase());
+            return nameMatch || descriptionMatch;
+        });
+
+        console.log(results);
+    }
+
     return (
         <div className="search">
-            <input type="text" placeholder="🔍︎ Buscar Producto..."></input>
-            <button><a href="/products"><img src="https://i.postimg.cc/MHh7wH9S/lupa1.png" alt="lupa de busqueda"></img></a></button>
+            <input type="text" placeholder="🔍︎ Buscar Producto..." value={inputValue} onChange={handleInputChange}></input>
+            <button onClick={() => searchByString()}><img src="https://i.postimg.cc/MHh7wH9S/lupa1.png" alt="lupa de busqueda"></img></button>
         </div>
     )
 
