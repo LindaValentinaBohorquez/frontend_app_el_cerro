b52f091a8403cd9d97e7d236398a1d3dc710cebc
LindaBohorquez <lindavalentinabh@ufps.edu.co>
2023-12-19 17:57:44 -0500

    Actualizacion de productos y promociones

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Bar from './components/shared/bar/Bar';
 import Footer from './components/shared/footer/Footer';
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import Landing from './components/landing/Landing';
 import ListProducts from './components/list_products/ListProducts';
 import Step1 from './components/payment/step1/Step1';
@@ -12,9 +12,10 @@ import Payment from './components/payment/Payment';
 import SectionCards from './components/landing/section_cards/SectionCards';
 import { Toaster, toast } from 'sonner';
 import useFetch from './hooks/useFetchData';
+import ListProductsDiscount from './components/list_products/ListProductDiscount';
 
 function App() {
-  const { data: products, loading, error } = useFetch("http://localhost:40571/AppElCerro/resources/producto/productos");
+  const { data: products, error } = useFetch("http://localhost:40571/AppElCerro/resources/producto/productos");
   if (error != null) {
     toast.error(error);
   }
@@ -22,27 +23,22 @@ function App() {
 
   return (
     <div className="App">
-      {loading ?
-        <div>
-          {loading}
-        </div>
-        :
-        <div>
-          <Toaster />
-          <Bar />
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/products" element={<ListProducts products={products} />} />
-            <Route path="/products/:tipo" element={<ListProducts products={products} />} />
-            <Route path="/SectionCards" element={<SectionCards />} />
-            <Route path="/payment/step1" element={<Step1 />} />
-            <Route path="/payment/step2" element={<Step2 />} />
-            <Route path="/payment/step3" element={<Step3 />} />
-            <Route path="/payment" element={<Payment />} />
-          </Routes>
-          <Footer />
-        </div>
-      }
+      <Toaster />
+      <Bar />
+      <Outlet />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/products" element={<ListProducts products={products} />} />
+        <Route path="/products/:tipo" element={<ListProducts products={products} />} />
+        <Route path="/productsDiscount" element={<ListProductsDiscount />} />
+        <Route path="/productsDiscount/:tipo" element={<ListProductsDiscount />} />
+        <Route path="/SectionCards" element={<SectionCards />} />
+        <Route path="/payment/step1" element={<Step1 />} />
+        <Route path="/payment/step2" element={<Step2 />} />
+        <Route path="/payment/step3" element={<Step3 />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/list_products/ListProductDiscount.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import CardProduct from './card_discount/CardProductDiscount';
+import './ListProductDiscount.css';
+import { useParams } from 'react-router-dom';
+
+function ListProductsDiscount() {
+  const [data, setData] = useState([]);
+  const { tipo } = useParams();  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let apiUrl = 'http://localhost:40571/AppElCerro-master/resources/detallexpromo/detallesxpromos';
+        if (tipo) {
+          apiUrl = `http://localhost:40571/AppElCerro-master/resources/detallexpromo/detallesxpromos/${tipo}`;
+        }
+
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error('Error al obtener los datos');
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, [tipo]);
+  return (
+    <div className="listp-product-discount-title">
+      <div className="listp-product-discount-title">
+        <h2>Inicio &gt; Lista de productos descuentos</h2>
+      </div>
+      <div className="listp-product-discount-title2">
+        <h2>Listado de productos con descuento</h2>
+      </div>
+      <div className="list-grid">
+        {data.map(productDiscount => (
+          <CardProduct
+            key={productDiscount.id_producto}
+            name={productDiscount.nombre_producto}
+            description={productDiscount.descripcion}
+            discount={productDiscount.descuento}
+            discountPrice={productDiscount.precio_descuento}
+            price={productDiscount.precio_sin_descuento}
+            url={productDiscount.img}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ListProductsDiscount;
