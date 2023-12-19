e24f5e8d85d47793d4e5550facecd3002eccb70c
LindaBohorquez <lindavalentinabh@ufps.edu.co>
2023-12-19 16:51:56 -0500

    se actualiza app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Footer from './components/shared/footer/Footer';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import Landing from './components/landing/Landing';
 import ListProducts from './components/list_products/ListProducts';
-import ListProductsDiscount from './components/list_products/ListProductDiscount';
 import Step1 from './components/payment/step1/Step1';
 import Step2 from './components/payment/step2/Step2';
 import Step3 from './components/payment/step3/Step3';
 import Payment from './components/payment/Payment';
 import SectionCards from './components/landing/section_cards/SectionCards';
-import { Toaster} from 'sonner';
+import { Toaster, toast} from 'sonner';
+import useFetch from './hooks/useFetchData';
 
 function App() {
    const { data: products, error } = useFetch("http://localhost:8080/AppElCerro/resources/producto/productos");
@@ -18,42 +18,16 @@ function App() {
     toast.error(error);
   }
   
-  localStorage.setItem('allProducts', JSON.stringify(data));
-
- /* const { data: products, error } = useFetch("http://localhost:40571/AppElCerro/resources/producto/productos");
-  if (error != null) {
-    toast.error(error);
-  }
-  localStorage.setItem('allProducts', JSON.stringify(products));*/
-
-  const navigate = useNavigate();
-
-  const redirectToProducts = () => {
-    navigate('/products');
-  };
-
-  const handleNextStep = () => {
-
-  };
-
-  const formData = {
-    nombre: '',
-    direccion: '',
-    email: '',
-    telefono: '',
-  };
-
-  const { tipo } = useParams();
+  localStorage.setItem('allProducts', JSON.stringify(products));
 
   return (
     <div className="App">
       <Toaster />
       <Bar />
-      <Outlet /> 
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/products" element={<ListProducts />} />
-        <Route path="/products/:tipo" element={<ListProducts />} />
+        <Route path="/products" element={<ListProducts products={products}/>} />
+        <Route path="/products/:tipo" element={<ListProducts products={products}/>} />
         <Route path="/SectionCards" element={<SectionCards />} />
         <Route path="/payment/step1" element={<Step1 />} />
         <Route path="/payment/step2" element={<Step2 />} />
